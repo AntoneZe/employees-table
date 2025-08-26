f7a5d896d2506cb981de5968b910f7b6c5265888
Anton Zinchenko <D-krabike@yandex.ru>
2025-08-26 15:51:13 +0500

    chore: init project with MRT table & MSW

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { PageEmployees } from '@widgets/EmployeesTablePage/PageEmployees'
 
-const router = createBrowserRouter([
-  { path: '/', element: <PageEmployees /> },
-])
+const router = createBrowserRouter(
+  [{ path: '/', element: <PageEmployees /> }],
+  { basename: import.meta.env.BASE_URL } 
+)
 
 export function AppRoutes() {
   return <RouterProvider router={router} />
